be65567d242f4c5f1a4533beb68e3b9192294887
Anamitra <anamitrahazra88915@gmail.com>
2024-09-22 11:03:29 +0530

    Configured for deployment

--- mern-estate/api/index.js
+++ mern-estate/api/index.js
@@ -6,7 +6,6 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';  // Import CORS
-import path from 'path';
 
 dotenv.config();
 
@@ -19,8 +18,6 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
-
 const app = express();
 
 // Configure CORS to allow requests from https://rentnest.vercel.app
@@ -30,23 +27,14 @@ app.use(cors({
 }));
 
 app.use(express.json());
-
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
-
+// Routes for API
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
-
+// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
@@ -55,4 +43,9 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
--- mern-estate/client/vite.config.js
+++ mern-estate/client/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         // target: 'http://localhost:3000',
         target: 'https://rentnest-xhq8.onrender.com/',
         secure: true,
+        changeOrigin: true,
       },
     },
   },
